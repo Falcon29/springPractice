e88bf3094d1b623b062ac096dddbc32bd595869a
Falcon29 <39822571+Falcon29@users.noreply.github.com>
2021-01-28 12:54:31 +0300

    Add files via upload

---
+++ springPractice/src/main/java/ru/ache/springpractice/TestBean.java
@@ -0,0 +1,35 @@
+/*
+ *
+ * =======================================================================
+ *
+ * Copyright (c) 2009-2021 Sony Network Entertainment International, LLC. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Sony Network Entertainment International, LLC.
+ * You shall not disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into with
+ * Sony Network Entertainment International, LLC.
+ *
+ * =======================================================================
+ *
+ * For more information, please see http://www.sony.com/SCA/outline/corporation.shtml
+ *
+ */
+
+package ru.ache.springpractice;
+
+public class TestBean {
+    private String name;
+
+    public TestBean( String name ) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+}
