52488e96a9d990c8cc23ecc706b34945a562e902
anton.chuprov <anton.chuprov@sperasoft.com>
2021-02-03 09:44:14 +0300

    [lesson 11] start

--- src/main/java/ru/ache/springpractice/MusicPlayer.java
+++ src/main/java/ru/ache/springpractice/MusicPlayer.java
@@ -28,7 +28,6 @@ import org.springframework.stereotype.Component;
 public class MusicPlayer {
     private SalsaMusic salsaMusic;
     private TimbaMusic timbaMusic;
-    //@Autowired //внедрение зависимости через поле
     private Music music;
 
     @Autowired
@@ -37,92 +36,8 @@ public class MusicPlayer {
         this.timbaMusic = timbaMusic;
     }
 
-    /** урок 10 ч.1
-    //Spring будет искать класс, помеченный Компонент
-    @Autowired
-    public MusicPlayer(SalsaMusic salsaMusic) {
-        this.salsaMusic = salsaMusic;
-    }
-
-    public void playMusic() {
-        System.out.println( "Playing: " + salsaMusic.getSong() );
-    }
-    **/
-
-    /** Lesson 10 p.1 - внедрение зависимости через конструктор и через сеттер
-    //@Autowired
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    //@Autowired
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-     **/
-
     public String playMusic() {
-        //System.out.println( "Playing: " + salsaMusic.getSong() );
-        //System.out.println( "Playing: " + timbaMusic.getSong() );
         return "Playing: " + salsaMusic.getSong();
     }
 
-    /** LESSONS 1-9
-    private Music music;
-    private String name;
-    private int volume;
-    private int queue;
-
-    private List<Music> musicList = new ArrayList<>();
-
-    //IoC
-    public MusicPlayer( Music music ) {
-        this.music = music;
-    }
-
-    //DI
-    public MusicPlayer() {}
-
-    public void setMusic( Music music ) {
-        this.music = music;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName( String name ) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume( int volume ) {
-        this.volume = volume;
-    }
-
-    public int getQueue() {
-        return queue;
-    }
-
-    public void setQueue( int queue ) {
-        this.queue = queue;
-    }
-
-    public void setMusicList( List<Music> musicList ) {
-        this.musicList = musicList;
-    }
-
-    public void playMusic() {
-        System.out.println( "Playing... : " + music.getSong() );
-    }
-
-    public void playAllMusic() {
-        for ( Music song : musicList ) {
-            System.out.println( "Playing : " + song );
-        }
-    }
-     LESSONS 1-9 **/
 }
--- src/main/java/ru/ache/springpractice/TestSpring.java
+++ src/main/java/ru/ache/springpractice/TestSpring.java
@@ -28,61 +28,8 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        /** THIS IS LESSONS 1-8 ---UNCOMMENT THIS
-
-        //Lesson 1-2 intro
-        //Music music = context.getBean( "musicBean", Music.class );
-        //Music musicJazz = context.getBean( "jazzMusicBean", Music.class );
-        //MusicPlayer musicPlayer = new MusicPlayer( music );
-        //MusicPlayer musicPlayerJazz = new MusicPlayer( musicJazz );
-
-        //Singleton
-        MusicPlayer musicPlayer = context.getBean( "musicPlayer", MusicPlayer.class );
-        MusicPlayer secondMusicPlayer = context.getBean( "musicPlayer", MusicPlayer.class );
-
-        System.out.println( musicPlayer == secondMusicPlayer ); //true
-        musicPlayer.setVolume( 10 );
-        secondMusicPlayer.getVolume(); //10 'cause SINGLETON
-
-        musicPlayer.playMusic();
-        //musicPlayerJazz.playMusic();
-        System.out.println( musicPlayer.getName() );
-        System.out.println( musicPlayer.getVolume() );
-        System.out.println( musicPlayer.getQueue() );
-
-        //Prototype
-        MusicPlayer musicPlayerProto = context.getBean( "musicPlayerPrototype", MusicPlayer.class );
-        MusicPlayer secondMusicPlayerProto = context.getBean( "musicPlayerPrototype", MusicPlayer.class );
-
-        System.out.println( musicPlayerProto == secondMusicPlayerProto ); //false
-        musicPlayerProto.setVolume(10);
-        musicPlayerProto.getVolume();  //10 'cause Prototype
-        secondMusicPlayerProto.getVolume();  //50 'cause Prototype and setted in properties/appContext.xml
-
-        //Lifecycle of bean
-        RockMusic rockMusic = context.getBean( "musicBean", RockMusic.class );
-
-        System.out.println( rockMusic.getSong() );
-
-         THIS IS THE END OF 1-8 LESSONS **/
-
-        /** LESSON 9
-        //ANNOTATIONS
-        Music salsaMusic = context.getBean( "salsaId", Music.class );
-        MusicPlayer salsaPlayer = new MusicPlayer(salsaMusic);
-        salsaPlayer.playMusic();
-
-        Music timbaMusic = context.getBean( "timbaMusic", Music.class );
-        MusicPlayer timbaPlayer = new MusicPlayer(timbaMusic);
-        timbaPlayer.playMusic();
-         lesson 9 **/
-
-        //AUTOWIRED lesson 10
-        //MusicPlayer musicPlayer = context.getBean( "musicPlayer", MusicPlayer.class );
-        //musicPlayer.playMusic();
-
         Computer computer = context.getBean( "computer", Computer.class );
-        System.out.println(computer);   //Computer=>MusicPlayer->salsaMusic / ->timbaMusic
+        System.out.println(computer);
 
         context.close();
     }
